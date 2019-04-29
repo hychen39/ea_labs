f2605add19937317e6fab483dd28cd6034a26d06
hychen39-T460S <hychen39@gmail.com>
2019-04-29 22:07:10 +0800

    Add ua_unit07_demo. Use facade pattern for the CRUD operations.

---
+++ ua_unit07_demo/src/java/entityControler/CustomerJPA.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entityControler;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceUnit;
+
+/**
+ *
+ * @author hychen39@gm.cyut.edu.tw
+ */
+@Stateless
+public class CustomerJPA {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    @PersistenceUnit(unitName = "ua_unit07_demoPU")
+    private EntityManager em;
+
+    public EntityManager getEm() {
+        return em;
+    }
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+    
+    
+}
